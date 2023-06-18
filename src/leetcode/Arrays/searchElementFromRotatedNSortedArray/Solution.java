8d6f4dbb7f8fccd1b5522347674d8177bad9e237
kudtarkarsahil <sahilkudtarkar315@gmail.com>
2023-06-18 22:09:17 +0530

    #33 array stack

---
+++ src/leetcode/Arrays/searchElementFromRotatedNSortedArray/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.Arrays.searchElementFromRotatedNSortedArray;
+
+public class Solution {
+    public static int searchRotatedNSortedArray(int []nums, int target) {
+        int start=0; int end=nums.length-1;
+        while(start <= end) {
+            int mid = (start+end)/2;
+            if(nums[mid] == target) {
+                 return mid;
+            }
+            if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && nums[mid] >= target) {
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
+            }
+            else {
+                if(nums[end] >= target && nums[mid] < target) {
+                    start = mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int []n = {4,5,6,7,0,1,2};
+        int target = 0;
+        int result = searchRotatedNSortedArray(n, target);
+        System.out.println(result);
+    }
+}
